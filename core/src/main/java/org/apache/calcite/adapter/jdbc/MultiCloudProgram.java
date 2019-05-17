1fef6be054679e42b70b2de1a1a6ae4791d18d03
igrgurina <ivan.grgurina@gmail.com>
2019-05-17 13:43:00 +0200

    Removed program call instead of disabling rule.

--- core/src/main/java/org/apache/calcite/adapter/jdbc/MultiCloudProgram.java
+++ core/src/main/java/org/apache/calcite/adapter/jdbc/MultiCloudProgram.java
@@ -23,20 +23,10 @@ public class MultiCloudProgram implements Program {
 
     @Override
     public RelNode run(RelOptPlanner planner, RelNode rel, RelTraitSet requiredOutputTraits, List<RelOptMaterialization> materializations, List<RelOptLattice> lattices) {
-        // TODO: remove this after testing; this is at the moment used for comparison to check if relVisitor behaves in the same way so that we can replace this with RelVisitor if it does.
-        RelNode data = Programs.hep(Collections.singleton(MultiCloudRuleManager.MultiCloudDataCollector.INSTANCE), false, null)
-                .run(planner, rel, requiredOutputTraits, materializations, lattices);
-
         // TODO: make use of this
-        // EXPLAIN: run standard program to get "optimized" query on original database, since then we can guarantee some stuff, like filter into join and stuff.
-        // MultiCloudDataManager.findFields(Programs.standard().run(planner, data, requiredOutputTraits, materializations, lattices));
-        MultiCloudDataManager.findFields(data);
+        MultiCloudDataManager.findFields(rel);
 
-        Program hep = Programs.hep(MultiCloudRuleManager.rules(), false, null);
-
-        RelNode run = hep.run(planner, data, requiredOutputTraits, materializations, lattices);
-
-        return run;
+        return rel;
     }
 }
 
