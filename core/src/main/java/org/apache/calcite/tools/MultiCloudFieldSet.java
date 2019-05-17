9bd64b41a3b947f93631a95da8582b5a1f631341
igrgurina <ivan.grgurina@gmail.com>
2019-05-17 14:00:50 +0200

    Commit before package change

---
+++ core/src/main/java/org/apache/calcite/tools/MultiCloudFieldSet.java
@@ -0,0 +1,8 @@
+package org.apache.calcite.tools;
+
+import org.apache.calcite.adapter.jdbc.MultiCloudField;
+
+// following the RuleSet principle
+public interface MultiCloudFieldSet extends Iterable<MultiCloudField>{
+
+}
